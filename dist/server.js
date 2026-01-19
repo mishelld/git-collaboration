e04d0ad2e8be038686b16e7a23782bbc6c16ce74
galraij <galraij@gmail.com>
2026-01-19 17:05:01 +0200

    I've added a secret message

---
+++ dist/server.js
@@ -0,0 +1,14 @@
+// Hi Michelle!
+
+const run = true
+
+
+if (run) {
+    let distance = 8
+    for (var i = 0; i < distance; i++) {
+        console.log("running") // print running 8 times
+    }
+    console.log("Finished running " + distance + " miles") //error - you can fix it by putting let distance at the beggining of the code
+}
+
+console.log("Damn, you see this gal? She ran " + distance + " miles")
